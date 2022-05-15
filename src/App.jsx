03283e6b2453575e22fba8aa4955575721cde7c4
Abdullah Al Noman <kazinoman9999@gmail.com>
2022-05-15 23:59:27 +0600

    add some http method

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import { Button, Divider } from "@mantine/core";
+import { Container } from "@mantine/core";
+import axios from "axios";
+import { Grid, Typography } from "@mui/material";
+import "./App.css";
+
+const apiEndPoint = "https://jsonplaceholder.typicode.com/posts";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [getData, setData] = useState([]);
+
+  const getSomeData = async () => {
+    const data = await axios.get(apiEndPoint);
+    setData(data.data);
+    console.log(getData);
+  };
+
+  // add data handler
+
+  const addHandler = async () => {
+    const obj = { title: "a", body: "b" };
+    const { data: post } = await axios.post(apiEndPoint, obj);
+    const posts = [post, ...getData];
+    setData(posts);
+  };
+
+  // update data
+
+  const updatePost = async (data) => {
+    // const post = { ...data, title: "asd" };
+    data.title = "update";
+    await axios.put(apiEndPoint + "/" + data.id, data);
+    // console.log(updateData);
+
+    const posts = [...getData];
+    const index = posts.indexOf(data);
+    posts[index] = { ...data };
+    setData(posts);
+  };
+
+  useEffect(() => {
+    getSomeData();
+  }, []);
+
+  if (!getData) {
+    return (
+      <>
+        <h1>Loading..........</h1>
+      </>
+    );
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello world !!!</h1>
+      {/* <button>get data</button> */}
+      <Container size={200} px={0}>
+        <Button
+          variant="gradient"
+          gradient={{ from: "indigo", to: "cyan" }}
+          onClick={addHandler}
+        >
+          Add data
+        </Button>
+      </Container>
+      <Grid container>
+        <Grid item xs={8}>
+          <Typography>Title</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          Update
+        </Grid>
+        <Grid item xs={2}>
+          Delete
+        </Grid>
+      </Grid>
+      <Divider
+        sx={{
+          marginTop: "1.5rem",
+          marginBottom: "1.5rem",
+          height: "0.2rem",
+          background: "#0057",
+        }}
+      />
+      {getData?.map((data) => (
+        <div style={{ borderBottom: "1px dotted black" }}>
+          <Grid container spacing={3} sx={{ my: 1 }} alignItems="center">
+            <Grid item xs={8}>
+              <Typography>{data.title}</Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Button
+                onClick={() => {
+                  updatePost(data);
+                }}
+              >
+                Update
+              </Button>
+            </Grid>
+            <Grid item xs={2}>
+              <Button color="red">Delete</Button>
+            </Grid>
+            <Divider />
+            {/* <Divider
+              sx={{
+                // marginTop: "1.5rem", 
+                // marginBottom: "1.5rem",
+                height: "0.2rem",
+                background: "#0057",
+              }}
+            /> */}
+          </Grid>
+        </div>
+      ))}
+      <Divider />
+    </div>
+  );
+}
+
+export default App;
